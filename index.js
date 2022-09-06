f07f9434c631c60d926460acfd0dbfc23f1236c0
Lornakaboro <lornakaboro@gmail.com>
2022-09-06 23:18:56 +0300

    Add blur efffect to mobile menu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function toggleMobileMenu() {
     document.getElementById("nav-dropdown-menu").classList.toggle("hidden-dropdown-menu")
+    document.getElementById("intro-section").classList.toggle("blur-intro-section")
 }
 
 function openDropdownItem(menuElement, menuLink) {
