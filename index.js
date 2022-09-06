f318e8fb2b9e658d8b9a06801f52065b990f2288
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-06 15:12:54 +0200

    Make nav-menu interactive

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function toggleMobileMenu() {
     document.getElementById("nav-dropdown-menu").classList.toggle("hidden-dropdown-menu")
 }
 
-
-
-
+function openDropdownItem(menuElement, menuLink) {
+    document.getElementById("nav-dropdown-menu").classList.toggle("hidden-dropdown-menu")
+    menuElement.href= menuLink
+}
 
