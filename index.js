5aa0ae2ab6506c89d211b1db7ed26206a62377c8
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-09 18:01:32 +0200

    Start in validation by js

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function openDropdownItem(menuElement, menuLink) {
     menuElement.href= menuLink
 }
 
+const form = document.querySelector('.contact-form-content');
+const email = document.querySelector('.contact-form-email');
+const error = document.querySelector('.contact-form-email + span.error')
+const upperCaseCheck = () => {
+    if (email.value !== email.value.toLowerCase()) {
+      return true;
+    }
+    return false;
+  };
+
+email.addEventListener('input', () => {
+    if(email.validity.valid && !upperCaseCheck()){
+        error.textContent = ''
+        error.className = 'error'
+    }
+})
