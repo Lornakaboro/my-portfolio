eb8e4101189965046e41a646b54064578a962768
Lornakaboro <lornakaboro@gmail.com>
2022-09-09 18:30:30 +0300

    Adjust required changes

--- index.js
+++ index.js
@@ -1,68 +1,80 @@
 function toggleMobileMenu() {
-    document.getElementById("nav-dropdown-menu").classList.toggle("hidden-dropdown-menu")
-    document.getElementById("intro-section").classList.toggle("blur-intro-section")
+  document
+    .getElementById("nav-dropdown-menu")
+    .classList.toggle("hidden-dropdown-menu");
+  document
+    .getElementById("intro-section")
+    .classList.toggle("blur-intro-section");
 }
 
 function openDropdownItem(menuElement, menuLink) {
-    document.getElementById("nav-dropdown-menu").classList.toggle("hidden-dropdown-menu")
-    document.getElementById("intro-section").classList.toggle("blur-intro-section")
-    menuElement.href= menuLink
+  document
+    .getElementById("nav-dropdown-menu")
+    .classList.toggle("hidden-dropdown-menu");
+  document
+    .getElementById("intro-section")
+    .classList.toggle("blur-intro-section");
+  menuElement.href = menuLink;
 }
 
 function getTechnologies(technologies) {
-    let tech = ""
-    for(let i =0; i<technologies.length; i++){
-        const currentTechnology = technologies[i]
-        tech += `<li class="tag">${currentTechnology}</li>`
-    }
-    return tech
+  let tech = "";
+  for (let i = 0; i < technologies.length; i++) {
+    const currentTechnology = technologies[i];
+    tech += `<li class="tag">${currentTechnology}</li>`;
+  }
+  return tech;
 }
 
 function closePopUp() {
-    popup.classList.add("hide-mobile-popup")
+  popup.classList.add("hide-mobile-popup");
 }
 
 const projectData = [
-    {
-        name: "Tonic",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        featuredImage: "./images/tonic-work.png",
-        technologies: ["html", "css", "javascript"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Multi-Post Stories",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        featuredImage: "./images/multi-post-stories.png",
-        technologies: ["html", "css", "javascript"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Tonic",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        featuredImage: "./images/tonic-work2.png",
-        technologies: ["html", "css", "javascript"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Multi-Post Stories",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        featuredImage: "./images/multi-post-stories2.png",
-        technologies: ["html", "css", "javascript"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-]
+  {
+    name: "Tonic",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featuredImage: "./images/tonic-work.png",
+    technologies: ["html", "css", "javascript"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Multi-Post Stories",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featuredImage: "./images/multi-post-stories.png",
+    technologies: ["html", "css", "javascript"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Tonic",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featuredImage: "./images/tonic-work2.png",
+    technologies: ["html", "css", "javascript"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Multi-Post Stories",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featuredImage: "./images/multi-post-stories2.png",
+    technologies: ["html", "css", "javascript"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+];
 
 function generateMobileProjectData(projectList) {
-    let data = "";
-    for(let i = 0; i<projectList.length; i++){
-        const currentProject = projectData[i]
-        const technologies = getTechnologies(currentProject.technologies)
-        data += `
+  let data = "";
+  for (let i = 0; i < projectList.length; i++) {
+    const currentProject = projectData[i];
+    const technologies = getTechnologies(currentProject.technologies);
+    data += `
             <div class="work">
                 <img src="${currentProject.featuredImage}" class="work-image" alt="multi-post-stories2">
                 <h2 class="work-title">${currentProject.name}</h2>
@@ -81,20 +93,19 @@ function generateMobileProjectData(projectList) {
                     See Project
                 </a>
             </div>
-        `
-    }
-    return data;
+        `;
+  }
+  return data;
 }
 
-document.getElementById("mobile-works").innerHTML = generateMobileProjectData(projectData)
+document.getElementById("mobile-works").innerHTML =
+  generateMobileProjectData(projectData);
 
-const popup = document.getElementById("mobile-popup")
+const popup = document.getElementById("mobile-popup");
 
-function openPopUp(
-    index
-) {
-    const technologiesUsed = getTechnologies(projectData[index].technologies)
-    const data = `
+function openPopUp(index) {
+  const technologiesUsed = getTechnologies(projectData[index].technologies);
+  const data = `
     <div class="work">
         <div class="mobile-popup-title">
             <h2 class="work-title">${projectData[index].name}</h2>
@@ -123,73 +134,77 @@ function openPopUp(
             </a>
         </div>
     </div>
-    `
-    popup.innerHTML = data
-    popup.classList.remove("hide-mobile-popup")
+    `;
+  popup.innerHTML = data;
+  popup.classList.remove("hide-mobile-popup");
 }
 
 // desktop
-const desktopPopUp = document.getElementById("desktop-popup")
+const desktopPopUp = document.getElementById("desktop-popup");
 function closeDesktopPopUp() {
-    desktopPopUp.classList.add("hide-desktop-popup")
+  desktopPopUp.classList.add("hide-desktop-popup");
 }
 
 const desktopProjectData = [
-    {
-        name: "Tonic",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        featuredImage: "./images/tonic-desktop.png",
-        pointersTitle: "CANOPY",
-        pointersLevel: "Back End Dev",
-        pointersYear: "2015",
-        technologies: ["html", "css", "javascript"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Multi-Post Stories",
-        description: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-        featuredImage: "./images/multi-post-stories-desktop.png",
-        pointersTitle: "FACEBOOK",
-        pointersLevel: "Full Stack Dev",
-        pointersYear: "2015",
-        technologies: ["html", "css", "javascript", "Ruby on rails"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Facebook 360",
-        description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-        featuredImage: "./images/facebook.png",
-        pointersTitle: "FACEBOOK",
-        pointersLevel: "Full Stack Dev",
-        pointersYear: "2015",
-        technologies: ["html", "css", "javascript", "Ruby on rails"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-    {
-        name: "Uber Navigation",
-        description: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-        featuredImage: "./images/uber-navigation.png",
-        pointersTitle: "Uber",
-        pointersLevel: "Lead Developer",
-        pointersYear: "2018",
-        technologies: ["html", "css", "javascript", "Ruby on rails"],
-        linkToLiveVersion: "#!",
-        linkToSource: "#!"
-    },
-]
+  {
+    name: "Tonic",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featuredImage: "./images/tonic-desktop.png",
+    pointersTitle: "CANOPY",
+    pointersLevel: "Back End Dev",
+    pointersYear: "2015",
+    technologies: ["html", "css", "javascript"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Multi-Post Stories",
+    description:
+      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    featuredImage: "./images/multi-post-stories-desktop.png",
+    pointersTitle: "FACEBOOK",
+    pointersLevel: "Full Stack Dev",
+    pointersYear: "2015",
+    technologies: ["html", "css", "javascript", "Ruby on rails"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Facebook 360",
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    featuredImage: "./images/facebook.png",
+    pointersTitle: "FACEBOOK",
+    pointersLevel: "Full Stack Dev",
+    pointersYear: "2015",
+    technologies: ["html", "css", "javascript", "Ruby on rails"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+  {
+    name: "Uber Navigation",
+    description:
+      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    featuredImage: "./images/uber-navigation.png",
+    pointersTitle: "Uber",
+    pointersLevel: "Lead Developer",
+    pointersYear: "2018",
+    technologies: ["html", "css", "javascript", "Ruby on rails"],
+    linkToLiveVersion: "#!",
+    linkToSource: "#!",
+  },
+];
 
 function generateDesktopProjectData(projectList) {
-    let data = "";
-    for(let i = 0; i<projectList.length; i++){
-        const currentProject = desktopProjectData[i]
-        let workReverse = ""
-        if(i % 2 != 0) {
-          workReverse = "work-reverse"
-        }
-        data += `
+  let data = "";
+  for (let i = 0; i < projectList.length; i++) {
+    const currentProject = desktopProjectData[i];
+    let workReverse = "";
+    if (i % 2 != 0) {
+      workReverse = "work-reverse";
+    }
+    data += `
             <div class="work ${workReverse}">
                 <img src="${currentProject.featuredImage}" class="work-image" alt="image of a website hero-section">
                 <div class="work-description-section">
@@ -212,20 +227,20 @@ function generateDesktopProjectData(projectList) {
                     </a>
                 </div>  
             </div>
-        `
-    }
-    return data;
+        `;
+  }
+  return data;
 }
 
-document.getElementById("desktop-works").innerHTML = generateDesktopProjectData(projectData)
+document.getElementById("desktop-works").innerHTML =
+  generateDesktopProjectData(projectData);
 
-function openDesktopPopUp(
-    counter
-) 
-{
-    const currentProjectData = desktopProjectData[counter]
-    const desktopTechnologies = getTechnologies(desktopProjectData[counter].technologies)
-    const data = `
+function openDesktopPopUp(counter) {
+  const currentProjectData = desktopProjectData[counter];
+  const desktopTechnologies = getTechnologies(
+    desktopProjectData[counter].technologies
+  );
+  const data = `
     <div class="desktop-popup" >
         <div class="desktop-popup-title">
             <h2 class="work-title">${currentProjectData.name}</h2>
@@ -258,9 +273,7 @@ function openDesktopPopUp(
             </div>  
         </div>  
     </div>
-    `
-    desktopPopUp.innerHTML = data
-    desktopPopUp.classList.remove("hide-desktop-popup")
+    `;
+  desktopPopUp.innerHTML = data;
+  desktopPopUp.classList.remove("hide-desktop-popup");
 }
-
-
