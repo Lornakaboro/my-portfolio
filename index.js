068a84660566946d7762e8242b09084e84775061
Lornakaboro <lornakaboro@gmail.com>
2022-09-10 20:23:55 +0300

    Add email validation

--- index.js
+++ index.js
@@ -262,3 +262,13 @@ function openDesktopPopUp(
     desktopPopUp.innerHTML = data
     desktopPopUp.classList.remove("hide-desktop-popup")
 }
+
+// form validation
+
+function validateContactForm() {
+  const userEmail = document.forms["my-contact-form"]["email"].value;
+  if (userEmail !== userEmail.toLowerCase()) {
+    document.getElementById("contact-form-error").innerHTML = "Your email should be in lowercase"
+    return false;
+  }
+}
